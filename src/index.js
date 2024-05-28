9f35ed526b74f71ea53bbe6dbc23ba316b79b91f
Priyanshu Gaur <gaur.priyanshu169@gmail.com>
2024-05-28 13:18:04 +0530

    analytics added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import rootReducer from "./reducer";
 import {configureStore} from "@reduxjs/toolkit"
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react"
 
 
 const store = configureStore({
