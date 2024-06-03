d3c4f2bcb2aef60895d766ef0d58f50f8e985b86
Priyanshu Gaur <gaur.priyanshu169@gmail.com>
2024-06-03 11:52:00 +0530

    analytics

--- src/index.js
+++ src/index.js
@@ -5,25 +5,33 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import rootReducer from "./reducer";
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react"
 
 
 const store = configureStore({
-  reducer:rootReducer,
+  reducer: rootReducer,
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-  <Provider store = {store}>
-    <BrowserRouter>
-        <App />
-        <Toaster/>
-      </BrowserRouter>
-  </Provider>
+  <>
+
+
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Toaster />
+          <Analytics />
+        </BrowserRouter>
+      </Provider>
+
+
+    </React.StrictMode>
+
+
     
-    
-  </React.StrictMode>
+  </>
 );
